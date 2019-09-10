5be97b40b442681ead938f3465183665617026f0
gutsy lin <gutsylin@outlook.com>
2019-09-10 23:51:07 +0800

    page modify

--- controller/register_controller.py
+++ controller/register_controller.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 
 from base import app
 
 @app.route('/register/ui/')
 def register_ui():
     return render_template('register.html')
+
+@app.route('/register/validate_name')
+def validate_name():
+    name = request.form.get('username')
+    return jsonify(name)
